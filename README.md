7f4425b7f1cd014455ea47fa0ff6646a2b20c3da
Praveen Dias <praveendias1180@gmail.com>
2021-12-12 11:44:31 +0530

    🚀B2 to S3.

---
+++ README.md
@@ -0,0 +1,13 @@
+# BB to S3 
+
+WordPress plugin **Continuous Delivery** to AWS S3.
+
+Zip the selected files and deliver the archive to a AWS S3 bucket on new **tag push** to Bitbucket private repository.
+Also the installed plugins will be able to see the update and pull the updated WordPress plugin.
+
+```
+git push bb --tags
+```
+
+# BitBucket Pipelines
+
---
+++ bb2s3.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:       My Basics Plugin
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            John Smith
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+
+// Your plugin functionality is implemented here.
+// With the help of the classes defined in the 'inc' folder.
+
+/**
+ * https://developer.wordpress.org/plugins/plugin-basics/header-requirements/
+ */
